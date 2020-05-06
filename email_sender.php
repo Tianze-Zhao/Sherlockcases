6bbd855fe4c408cf69e1503fad7c9ee4039271e0
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-06 22:58:02 +1000

    email sender function prototype

---
+++ email_sender.php
@@ -0,0 +1,28 @@
+<?php
+//Connect to email_list table
+$server = "localhost";
+$username = "u673269877_yilin";
+$database = "u673269877_db";
+$password = "yilin940911";
+    
+$conn = mysqli_connect($server,$username,$password,$database) or die('Error connecting to database server');
+
+echo "Database connected.\n";
+
+//Read all personal details
+$qry = "SELECT * FROM email_list";
+$result = mysqli_query($conn,$qry);
+while($row = mysqli_fetch_array($result)){
+    echo $row['first_name'] . ' ' . $row['last_name'] . ' ' . $row['email'] . '<br/>';
+}
+
+
+//Send customized emails
+$from = 'tom940911@gmail.com';
+$subject = $POST_['subject'];
+$content = $POST_['content'];
+
+//Close connection
+mysqli_close($connection);
+
+?>
