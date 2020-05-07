707e9aa3a315ead6a76f5b703574d8dce8c78447
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-07 22:22:56 +1000

    eliminate void rows

--- info_saver.php
+++ info_saver.php
@@ -3,21 +3,19 @@ $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 
-echo $first_name . "\n";
-
-$link = "localhost";
-$username = "u673269877_yilin";
-$database = "u673269877_db";
-$password = "yilin940911";
+$server = "(localdb)\MSSQLLocalDB";
+$username = '';
+$database = "email_list";
+$password = '';
     
 //Connecting to database
-$connection = mysqli_connect($link,$username,$password,$database) or die('Error connecting to database server');
+$connection = mysqli_connect($server,$username,$password,$database) or die('Error connecting to database server');
 
-        echo "Database connected.\n";
+echo "Database connected.\n";
 //The insert query
 $query = "INSERT INTO email_list (first_name, last_name, email) VALUES ('$first_name','$last_name','$email')";
   
-        echo "Query written.\n";
+echo "Query written.\n";
 //Executing query
 mysqli_query($connection, $query) or die('Error querying database');
 
