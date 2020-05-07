6b8bebf3d4ec157155ef7f5d29fb19927740ed4b
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-07 22:33:18 +1000

    complete sender

--- email_sender.php
+++ email_sender.php
@@ -10,17 +10,24 @@ $conn = mysqli_connect($server,$username,$password,$database) or die('Error conn
 echo "Database connected.\n";
 
 //Read all personal details
+
 $qry = "SELECT * FROM email_list";
 $result = mysqli_query($conn,$qry);
-while($row = mysqli_fetch_array($result)){
-    if($row['first_name'] != '' && $row['last_name'] != '' && $row['email'] != '')
-    echo $row['first_name'] . ' ' . $row['last_name'] . ' ' . $row['email'] . '<br/>';
-};
 
 //Send customized emails
+
 $from = 'tom940911@gmail.com';
 $subject = $POST_['subject'];
 $content = $POST_['content'];
+
+while($row = mysqli_fetch_array($result)){
+    if($row['first_name'] != '' && $row['last_name'] != '' && $row['email'] != '')
+    $to = $row['email'];
+    mail($to,$subject,$content,'From:'.$from);
+    echo 'Email sent to: ' . $to . '<br/>';
+};
+
+
 //Close connection
 mysqli_close($connection);
 
