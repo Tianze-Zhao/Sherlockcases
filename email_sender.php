157efca8c9f484ad538db719b45c84bbb386e2da
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-13 13:55:35 +1000

    if statement

--- email_sender.php
+++ email_sender.php
@@ -12,7 +12,7 @@
             <label for = 'content'>Body of email:</label><br/>
             <textarea id = 'content' name = 'content'></textarea><br/>
             
-            <input type = 'submit' value = 'Submit'/>
+            <input type = 'submit' name = 'submit' value = 'Submit'/>
             
         </form>
     </body>
@@ -24,31 +24,35 @@ $server = "localhost";
 $database = "u673269877_db";
 $username = 'u673269877_yilin';
 $password = 'yilin940911';
+
+if(isset($_POST['submit'])){
+
+    // Connect to database
+    $conn = mysqli_connect($server,$username,$password,$database) or die('Error connecting to database server');
+
+    echo "Database connected.<br/>";
+
+    //SELECT*FROM email_list
+
+    $qry = "SELECT * FROM email_list";
+    $result = mysqli_query($conn,$qry);
+
+    //Edit customized emails
+
+    $from = 'yilin@sherlockcases.com';
+    $subject = $_POST['subject'];
+    $content = $_POST['content'];
     
-$conn = mysqli_connect($server,$username,$password,$database) or die('Error connecting to database server');
+    //Send to everyone
+    while($row = mysqli_fetch_array($result)){
+        $to = $row['email'];
 
-echo "Database connected.<br/>";
+        mail($to,$subject,$content,'From:'.$from) or die("error");
+        echo 'Email sent to: ' . $to . '<br/>';
 
-//Read all personal details
+    };
 
-$qry = "SELECT * FROM email_list";
-$result = mysqli_query($conn,$qry);
-
-//Send customized emails
-
-$from = 'yilin@sherlockcases.com';
-$subject = $_POST['subject'];
-$content = $_POST['content'];
-
-while($row = mysqli_fetch_array($result)){
-    $to = $row['email'];
-
-    mail($to,$subject,$content,'From:'.$from) or die("error");
-    echo 'Email sent to: ' . $to . '<br/>';
-    
-};
-
-//Close connection
-mysqli_close($connection);
-
+    //Close connection
+    mysqli_close($connection);
+}
 ?>
