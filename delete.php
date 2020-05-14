651137af4ac4bd6c574f987cc2dbd894f723215f
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-14 23:27:32 +1000

    syntax

--- delete.php
+++ delete.php
@@ -15,8 +15,8 @@
         //Delete selected ids
         if(isset($_POST['submit'])){
             foreach($_POST['todelete'] as $delete_id){
-                $query = 'DELETE FROM email_list WHERE id = $delete_id';
-                mysqli_query(connection,$query) or die("Error querying database");
+                $query = "DELETE FROM email_list WHERE id = $delete_id";
+                mysqli_query($connection,$query) or die("Error querying database");
             }
             echo "Customer deleted.<br/>";
 
@@ -28,8 +28,8 @@
         while($row = mysqli_fetch_array($result)){
             echo '<input type="checkbox" value="'.$row['id'].'" name="todelete[]" />';
             echo $row['first_name'];
-            echo ''.$row['last_name'];
-            echo ''.$row['email'];
+            echo ' '.$row['last_name'];
+            echo ' '.$row['email'];
             echo '<br/>';
         }
         mysqli_close($connection);
