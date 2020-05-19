40ea6c94b1b805056a9206be35ed381dbe7b19d6
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-19 22:57:22 +1000

    adding image

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <form action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
   <label for='icon'>Please upload your favourite icon:</label>
   <input type='file' name='icon' id='icon'>
+  <image src='icon/<?php echo $file_name?>'>
   <input type='submit' value='submit'>
 </form>
 
 <?php
+$file_name = $_FILES['icon']['name'];
 $target = 'icon' . $_FILES['icon']['name'];
 move_uploaded_file($_FILES['icon']['tmp_name'],$target);
 
