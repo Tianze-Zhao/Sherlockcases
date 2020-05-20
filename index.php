e6db7eb627941171f9e87dd76b69135dfb799e4f
Tianze Zhao <s3573724@student.rmit.edu.au>
2020-05-20 12:18:43 +1000

    added enctype

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<form action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
+<form enctype='multipart/form-data' action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
   <label for='icon'>Please upload your favourite icon:</label>
   <input type='file' name='icon' id='icon'>
-  <image src='icon/<?php echo $file_name?>'>
-    <p><?php echo $file_name?></p>
+  <image src='icon/'>
   <input type='submit' value='submit'>
 </form>
 
 <?php
-$file_name = 'absd'.$_FILES['icon']['name'];
 $target = 'icon/' . $file_name;
 move_uploaded_file($_FILES['icon']['tmp_name'],$target);
 
